f55a94ec3ececd94fd7a42b4a8d87e94da853158
nclarke <nclarke>
2005-02-05 00:06:20 +0100

    major: added complete html parser. minor: spilt dtd stuff into 3 files.

---
+++ src/main/java/com/antiaction/common/html/HTMLProcessing.java
@@ -0,0 +1,33 @@
+/*
+ * Created on 02-02-2005
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package com.antiaction.common.html;
+
+/**
+ * @author Nicholas
+ *
+ * To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class HTMLProcessing extends HTMLItem {
+
+	private String tagText;
+	private String processing;
+
+	public HTMLProcessing(String tagText, String processing) {
+		this.tagText = tagText;
+		this.processing = processing;
+	}
+
+	public int getType() {
+		return HTMLItem.T_PROCESSING;
+	}
+
+	public String getText() {
+		return tagText;
+	}
+
+}
