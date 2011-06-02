a30a9c2c093cffc7d6499c5091eb7a139d11626c
nclarke <nclarke@antiaction.com>
2011-06-02 18:40:49 +0200

    Added support for encoded streams in html parser. Moved servlet debug streams to package.

---
+++ src/main/java/com/antiaction/common/html/HtmlInput.java
@@ -0,0 +1,16 @@
+/*
+ * Created on 02/06/2011
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+
+package com.antiaction.common.html;
+
+import java.io.IOException;
+
+public interface HtmlInput {
+
+	public int read() throws IOException;
+
+}
